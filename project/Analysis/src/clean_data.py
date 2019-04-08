45b4cb691df623cecf8c6810d3c9088556c5289d
Kedar Dabhadkar <dkedar@cmu.edu>
2019-04-08 10:33:53 -0700

    Featrue Extract complete (count+tfidf)

--- project/Analysis/src/clean_data.py
+++ project/Analysis/src/clean_data.py
@@ -1,10 +1,14 @@
 import numpy as np
 import os
 import sys
+import re
 
 from get_data import download
 
 from sklearn.externals import joblib
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 try:
     data = np.load("../Data/scratch_data.npz")
@@ -13,27 +17,50 @@ except:
     download()
     data = np.load("../Data/scratch_data.npz")
 
-train_text,train_labels= data['arr_0'], data['arr_1']
+train_text,train_labels = data['arr_0'], data['arr_1']
 test_text,test_labels = data['arr_2'], data['arr_3']
 
 def preprocess_text(text):
 
     # Rule 1
-    modified_text = " ".join(test_text[i].split("\n\n")[1:])
+    modified_text = " ".join(text.split("\n\n")[1:])
 
     # Rule 2
     if "--" in modified_text:
         modified_text =  " ".join(modified_text.split("--")[:-1])
 
     # Rule 3
+    stemmer = SnowballStemmer("english")
+    punctuations = '[! \" # $ % \& \' \( \) \ * + , \- \. \/ : ; <=> ? @ \[ \\ \] ^ _ ` { \| } ~]'
     modified_text = " ".join([stemmer.stem(X) for X in \
     (re.split(punctuations,text))])
 
     # Rule 4
-    modified_text = modified_test.replace('\n','').\
+    modified_text = modified_text.replace('\n','').\
     replace('\t','').replace('\r','').replace("  "," ")
 
     return modified_text
 
-def vectorize_text(text,word_dict):
-    return
+def vectorize_text():
+
+    train_text,train_labels= data['arr_0'], data['arr_1']
+    test_text,test_labels = data['arr_2'], data['arr_3']
+
+    train_text = [preprocess_text(text) for text in train_text]
+    test_text = [preprocess_text(text) for text in test_text]
+
+    vectorizer_count = CountVectorizer(stop_words ='english').fit(train_text)
+
+    train_countvector = vectorizer_count.transform(train_text)
+    test_countvector = vectorizer_count.transform(test_text)
+
+    vectorizer_tfidf = TfidfTransformer().fit(train_countvector)
+
+    train_tfidf = vectorizer_tfidf.transform(train_countvector)
+    test_tfidf = vectorizer_tfidf.transform(test_countvector)
+
+    return train_tfidf,test_tfidf
+
+# Write to file
+train_tfidf,test_tfidf = vectorize_text()
+np.savez("../Data/vectorized_data",train_tfidf.toarray(),train_labels,test_tfidf.toarray(),test_labels)
--- project/Analysis/src/get_data.py
+++ project/Analysis/src/get_data.py
@@ -4,12 +4,14 @@ import os
 import sys
 
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.externals import joblib
+
 classes = ['comp.graphics','comp.os.ms-windows.misc',
 'comp.sys.ibm.pc.hardware','comp.sys.mac.hardware']
 
 # Forming train and test data
 
-def download():
+def download(classes=classes):
 
     train = fetch_20newsgroups(data_home = "../Data", subset='train',
     categories=classes,
@@ -21,9 +23,11 @@ def download():
     test = fetch_20newsgroups(data_home = "../Data",
      subset='test',
      categories=classes,
-     huffle=True,
+     shuffle=True,
      random_state=42,
      remove=('headers','footers','quotes'))
+
+    data = joblib.load("../Data/20news-bydate_py3.pkz")
 
     train_text = [data['train'].data[i] for i in range(len(data['train'].data)) if \
     (data['train'].target_names)[data['train'].target[i]] in classes]
